18480efe0418fdf18c6e8a66af8462a430bac0a5
Sávio Issa de Sousa <2023101202010019@ifgoiano.local>
2026-03-12 07:54:59 -0300

    First commit

---
+++ stylohub/src/main/java/com/example/stylohub/StylohubApplication.java
@@ -0,0 +1,13 @@
+package com.example.stylohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StylohubApplication.class, args);
+	}
+
+}
---
+++ stylohub/src/test/java/com/example/stylohub/StylohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stylohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
